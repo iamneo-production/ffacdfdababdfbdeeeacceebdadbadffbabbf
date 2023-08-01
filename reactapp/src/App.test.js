9bad665c7afb17279cc7910921fdc1be1d3a1607
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:13:38 +0000

    Backup Commit

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import Stopwatch from "./components/Stopwatch/Stopwatch";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Stopwatch', () => {
+  render(<Stopwatch />);
+ 
 });
