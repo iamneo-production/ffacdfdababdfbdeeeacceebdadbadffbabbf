99a0c0150d9b31dde2a39035aab46bde0479c0c5
Iamneo <techadmin@iamneo.ai>
2023-08-01 12:58:38 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,9 +8,7 @@ function App() {
       <Stopwatch/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
